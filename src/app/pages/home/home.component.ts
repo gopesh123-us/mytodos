555c9e1c3bfa7ffc8b887464759e79b8cd6ed9ed
Gopesh Sharma <gopesh.sharma@gmail.com>
2025-01-21 00:55:20 -0500

    added padding to input box

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,7 +16,6 @@ export class HomeComponent {
     return this.tasks.length;
   }
   handleAddTask() {
-    console.log('task received');
     if (!this.tasks.includes(this.newTask)) {
       this.tasks.push(this.newTask);
     }
